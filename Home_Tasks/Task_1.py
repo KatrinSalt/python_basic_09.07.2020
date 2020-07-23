b5f3e8c2cc3df05ef81d1f408931cd6a69a9758d
KatrinSalt <59662534+KatrinSalt@users.noreply.github.com>
2020-07-23 17:21:26 +0200

    Lesson 4: Task 1

--- Home_Tasks/Task_1.py
+++ Home_Tasks/Task_1.py
@@ -12,6 +12,35 @@ from sys import argv
 script_name, work_hours, rate_per_hour, bonus = argv
 
 
-print(f'Первый параметр: {work_hours}')
+def is_number(a, b, c):
+    try:
+        int(a)
+        int(b)
+        int(c)
+        return True
+    except ValueError:
+        return False
+
+
+def employee_salary(hours, rate, bon):
+    salary = (hours * rate) + bon
+    return f"Зарплата сотрудника: {salary}"
+
+
+def my_func(hours, rate, bon):
+    if is_number(hours, rate, bon) is True:
+        hours = int(hours)
+        rate = int(rate)
+        bon = int(bon)
+        print(employee_salary(hours, rate, bon))
+    else:
+        print('Ошибка ввода. Введенные значения не являются числами. Повторите ввод.')
+
+
+my_func(work_hours, rate_per_hour, bonus)
+
+
+
+
 
 
