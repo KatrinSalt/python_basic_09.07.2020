eb47c7d862a9b03fed1f927806e205b10f392e76
KatrinSalt <59662534+KatrinSalt@users.noreply.github.com>
2020-07-28 13:38:56 +0200

    Lesson 5: Task 1

--- Home_Tasks/Task_1.py
+++ Home_Tasks/Task_1.py
@@ -5,4 +5,14 @@
 Выполнено Салтыковой Екатериной.
 27.08.20
 """
+file_name = input('Введите имя файла:\n')
+with open(f'{file_name}.txt', 'w') as user_file:
+    while True:
+        user_input = input('Введите любой текст: \n')
+        if len(user_input) == 0:
+            break
+        user_file.write(f'{user_input}\n')
 
+# with open("user_text.txt", "r") as user_file:
+#     content = user_file.read()
+#     print(content)
